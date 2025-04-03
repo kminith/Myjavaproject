84bbb35242331ffdd9629a83fccddb77e138a30c
kminith <minithkumar@puropalecreations.com>
2025-04-03 13:35:22 +0530

    secound commit

---
+++ MyJavaProject/src/main/java/ghm_mult.java
@@ -0,0 +1,9 @@
+public class Mult {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 2;
+        int result = num1 * num2;
+
+        System.out.println(num1 + " * " + num2 + " = " + result);
+    }
+}
